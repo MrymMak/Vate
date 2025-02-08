c853a7b2f173f858b08a250642eb94114c27f781
Milim-Creutzer <125588298+MrymMak@users.noreply.github.com>
2025-02-08 21:19:48 +0100

    Added code files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LoginSignupPage from "./pages/LoginSignupPage";
 import HomePage from "./pages/HomePage";
 import Docs from "./pages/Docs";
