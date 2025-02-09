d7f838b816e9e4adfc795b14214da9dffcc101d7
Milim-Creutzer <125588298+MrymMak@users.noreply.github.com>
2025-02-09 20:47:23 +0100

    Updates to fix template preview

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import LoginSignupPage from "./pages/LoginSignupPage";
 import HomePage from "./pages/HomePage";
 import Docs from "./pages/Docs";
@@ -7,6 +7,8 @@ import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
 import TechnicalDocs from "./pages/TechnicalDocs";
 import OverviewTopics from "./pages/OverviewTopics";
+import Template1 from "./pages/Template1"; // Import Template1 if needed
+
 
 // Placeholder components for missing pages
 const NotFound = () => <h1>404: Page Not Found</h1>;
@@ -17,6 +19,9 @@ const App = () => {
             <Routes>
                 {/* Auth Pages */}
                 <Route path="/" element={<LoginSignupPage />} />
+
+                {/* Template Route */}
+                <Route path="/templates/tech-docs/overview-topics/template1" element={<Template1 />} />
 
                 {/* Main Pages */}
                 <Route path="/home" element={<HomePage />} />
