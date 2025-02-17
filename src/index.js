3de6d992b84f7b7b23ab577b130a7e3b6b870bad
Milim-Creutzer <125588298+MrymMak@users.noreply.github.com>
2025-02-17 20:55:28 +0100

    Changes to fix api issue

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'antd/dist/reset.css'; // Ant Design styles reset
 import './App.css';          // Custom styles
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
