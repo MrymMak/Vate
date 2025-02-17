95ce79e07c2e04a2973e49c905927ae04e51fd18
Milim-Creutzer <125588298+MrymMak@users.noreply.github.com>
2025-02-17 20:59:00 +0100

    Api fixes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'antd/dist/reset.css'; // Ant Design styles reset
 import './App.css';          // Custom styles
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
